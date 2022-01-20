a9795cca92d72750bae491bbf5c13e6b3fe9e222
itbeard <iamitbeard@gmail.com>
2022-01-20 17:44:43 +0200

    update .net to v6 and remove scss dependency

--- Site/Itbeard.Site.WebAssembly/Program.cs
+++ Site/Itbeard.Site.WebAssembly/Program.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
@@ -12,7 +13,8 @@ namespace Itbeard.Site.WebAssembly
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddHeadElementHelper();

cb2b5bdf5feb01439e23c8fee1a1a5297c477a56
itbeard <iamitbeard@gmail.com>
2022-01-24 01:09:44 +0200

    add SSR on first loading

---
+++ Site/Itbeard.Site.Server/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services
+builder.Services.AddRazorPages();
+builder.Services.AddSingleton(sp =>
+{
+    // Get the address that the app is currently running at
+    var server = sp.GetRequiredService<IServer>();
+    var addressFeature = server.Features.Get<IServerAddressesFeature>();
+    var baseAddress = addressFeature.Addresses.First();
+    
+    return new HttpClient { BaseAddress = new Uri(baseAddress) };
+});
+builder.Services.AddHeadElementHelper();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+// Call UseBlazorFrameworkFiles on the app builder.
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+app.UseRouting();
+  
+// Change the fallback from the index.html file
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+    endpoints.MapControllers();
+    endpoints.MapFallbackToPage("/_Host");
+});
+
+app.Run();
--- Site/Itbeard.Site.WebAssembly/Program.cs
+++ Site/Itbeard.Site.WebAssembly/Program.cs
@@ -1,25 +1,9 @@
-using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 
-namespace Itbeard.Site.WebAssembly
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
-            builder.RootComponents.Add<HeadOutlet>("head::after");
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.Services.AddTransient(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddHeadElementHelper();
 
-            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddHeadElementHelper();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
+            

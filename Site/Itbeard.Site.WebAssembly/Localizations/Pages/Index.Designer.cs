2157cdedec364dccd4eddba168e025a853683918
itbeard <iamitbeard@gmail.com>
2022-09-11 00:12:16 +0200

    finalize localization of Index page

--- Site/Itbeard.Site.WebAssembly/Localizations/Pages/Index.Designer.cs
+++ Site/Itbeard.Site.WebAssembly/Localizations/Pages/Index.Designer.cs
@@ -74,5 +74,137 @@ namespace Itbeard.Site.WebAssembly.Localizations.Pages {
                 return ResourceManager.GetString("AboutShow_Paragraph_3", resourceCulture);
             }
         }
+        
+        internal static string Interview {
+            get {
+                return ResourceManager.GetString("Interview", resourceCulture);
+            }
+        }
+        
+        internal static string Interview_Paragraph_1 {
+            get {
+                return ResourceManager.GetString("Interview_Paragraph_1", resourceCulture);
+            }
+        }
+        
+        internal static string Interview_Paragraph_1_Button {
+            get {
+                return ResourceManager.GetString("Interview_Paragraph_1_Button", resourceCulture);
+            }
+        }
+        
+        internal static string Profession {
+            get {
+                return ResourceManager.GetString("Profession", resourceCulture);
+            }
+        }
+        
+        internal static string Profession_Paragraph_1 {
+            get {
+                return ResourceManager.GetString("Profession_Paragraph_1", resourceCulture);
+            }
+        }
+        
+        internal static string Profession_Paragraph_1_Button {
+            get {
+                return ResourceManager.GetString("Profession_Paragraph_1_Button", resourceCulture);
+            }
+        }
+        
+        internal static string Programming {
+            get {
+                return ResourceManager.GetString("Programming", resourceCulture);
+            }
+        }
+        
+        internal static string Programming_Paragraph_1 {
+            get {
+                return ResourceManager.GetString("Programming_Paragraph_1", resourceCulture);
+            }
+        }
+        
+        internal static string Programming_Paragraph_1_Button {
+            get {
+                return ResourceManager.GetString("Programming_Paragraph_1_Button", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies {
+            get {
+                return ResourceManager.GetString("ExtraGoodies", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Twitter_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Twitter_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Telegram_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Telegram_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Instagram_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Instagram_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Sponsorship_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Sponsorship_Title", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Sponsorship_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Sponsorship_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Patreon_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Patreon_Title", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Patreon_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Patreon_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Discord_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Discord_Title", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Discord_Content {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Discord_Content", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Instagram_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Instagram_Title", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Twitter_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Twitter_Title", resourceCulture);
+            }
+        }
+        
+        internal static string ExtraGoodies_Telegram_Title {
+            get {
+                return ResourceManager.GetString("ExtraGoodies_Telegram_Title", resourceCulture);
+            }
+        }
     }
 }

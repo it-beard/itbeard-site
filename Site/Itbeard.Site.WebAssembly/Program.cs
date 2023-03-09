78af4661402dd364948cbf77c1027232be07cace
itbeard <iamitbeard@gmail.com>
2023-03-09 02:27:17 +0100

    add 'lang' query param for hard language change

--- Site/Itbeard.Site.WebAssembly/Program.cs
+++ Site/Itbeard.Site.WebAssembly/Program.cs
@@ -1,8 +1,9 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Toolbelt.Blazor.Extensions.DependencyInjection;
 using System.Globalization;
+using System.Web;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
-
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.Services.AddTransient(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
@@ -11,19 +12,37 @@ builder.Services.AddLocalization();
 
 var host = builder.Build();
 
+#region Set Culture and Site language
 CultureInfo culture;
 var js = host.Services.GetRequiredService<IJSRuntime>();
 var result = await js.InvokeAsync<string>("blazorCulture.get");
-
 if (result != null)
 {
     culture = new CultureInfo(result);
 }
 else
 {
-    switch (CultureInfo.CurrentCulture.Name)
+    culture = await SetCulture(CultureInfo.CurrentCulture.Name, js);
+}
+
+// get lang from URL and rewrite culture
+var navManager = host.Services.GetRequiredService<NavigationManager>();
+var queryParams = HttpUtility.ParseQueryString(navManager.ToAbsoluteUri(navManager.Uri).Query);
+var langParam = queryParams["lang"];
+if (langParam != null)
+{
+    culture = await SetCulture(langParam, js);
+}
+
+CultureInfo.DefaultThreadCurrentCulture = culture;
+CultureInfo.DefaultThreadCurrentUICulture = culture;
+
+static async Task<CultureInfo> SetCulture(string lang, IJSRuntime js)
+{
+    CultureInfo culture;
+    switch (lang)
     {
-        case string s when s.StartsWith("be"):
+        case string s when s.StartsWith("by") || s.StartsWith("be"):
             culture = new CultureInfo("be");
             await js.InvokeVoidAsync("blazorCulture.set", "be");
             break;
@@ -40,10 +59,9 @@ else
             await js.InvokeVoidAsync("blazorCulture.set", "ru");
             break;
     }
-}
 
-CultureInfo.DefaultThreadCurrentCulture = culture;
-CultureInfo.DefaultThreadCurrentUICulture = culture;
+    return culture;
+}
+#endregion
 
 await host.RunAsync();
-            
